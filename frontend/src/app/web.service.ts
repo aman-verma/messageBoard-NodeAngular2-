7880729dde845295ec54077df8bfac45e0de8bd8
aman-verma <amanverma.verma530@gmail.com>
2018-01-07 12:02:06 +0530

    Create a data store in our service

--- frontend/src/app/web.service.ts
+++ frontend/src/app/web.service.ts
@@ -1,20 +1,32 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/toPromise';
-
+import { MatSnackBar } from '@angular/material';
 @Injectable()
 export class WebService {
     BASE_URL = 'http://localhost:63145/api';
     messages = [];
-    constructor(private http: Http) {
+    constructor(private http: Http, private sb: MatSnackBar ) {
         this.getMessages();
     }
     async getMessages() {
-       const response = await this.http.get( this.BASE_URL + '/messages').toPromise();
-       this.messages = response.json();
+        try {
+            const response = await this.http.get( this.BASE_URL + '/messages').toPromise();
+            this.messages = response.json();
+        } catch (error) {
+            this.handleError('unable to get messages');
+        }
     }
     async postMessage(message) {
-        const response = await this.http.post(this.BASE_URL + '/message', message ).toPromise();
-        this.messages.push(response.json());
+        try {
+            const response = await this.http.post(this.BASE_URL + '/message', message ).toPromise();
+            this.messages.push(response.json());
+        } catch (error) {
+            this.handleError('unable to post message');
+        }
+    }
+    private handleError(error) {
+            console.error(error);
+            this.sb.open(error, 'close' , {duration: 2000});
     }
 }
