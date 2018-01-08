a77670b4889cc801d8b9d9e8be6e599139416305
aman-verma <amanverma.verma530@gmail.com>
2018-01-08 22:41:13 +0530

    HTTP get with Observable

--- frontend/src/app/web.service.ts
+++ frontend/src/app/web.service.ts
@@ -7,15 +7,13 @@ export class WebService {
     BASE_URL = 'http://localhost:63145/api';
     messages = [];
     constructor(private http: Http, private sb: MatSnackBar ) {
-        this.getMessages();
+        // this.getMessages(name);
     }
-    async getMessages() {
-        try {
-            const response = await this.http.get( this.BASE_URL + '/messages').toPromise();
-            this.messages = response.json();
-        } catch (error) {
-            this.handleError('unable to get messages');
-        }
+    getMessages(user) {
+            user = (user) ? '/' + user : '';
+            this.http.get( this.BASE_URL + '/messages' + user).subscribe(response => {
+                this.messages = response.json();
+            }, error => {this.handleError('unable to get messages'); });
     }
     async postMessage(message) {
         try {
